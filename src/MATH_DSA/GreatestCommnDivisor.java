2a6e90453187e61543f1644930db9fa9292d2477
Saurabh Srivastav <saurabhsri.mau@gmail.com.>
2025-04-10 13:19:51 +0530

    euclidian gcd

--- src/MATH_DSA/GreatestCommnDivisor.java
+++ src/MATH_DSA/GreatestCommnDivisor.java
@@ -3,6 +3,7 @@ package MATH_DSA;
 public class GreatestCommnDivisor {
     public static void main(String[] args) {
    gcd(24,36);
+   euclidian(24,36);
     }
     //MAthematics approach
     public static void gcd(int num1,int num2){
@@ -14,6 +15,24 @@ public class GreatestCommnDivisor {
             }
             min--;
         }
+
+    }
+
+    //Euclidian  Algorithm
+    public static void euclidian(int num1,int num2){
+        while ((num1!=0 && num2!=0)){
+            if(num1>num2){
+                num1 -= num2;
+            }else {
+                num2 -= num1;
+            }
+        }
+        int res = num1;
+        if(num1 == 0) {
+            res = num2;
+        }
+
+        System.out.println("GCD is " + res);
 
     }
 }
