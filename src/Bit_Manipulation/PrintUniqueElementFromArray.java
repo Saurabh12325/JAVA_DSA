d0cd6c6bb54d8ce41c7450b6eb73e4788db347e0
Saurabh Srivastav <saurabhsri.mau@gmail.com.>
2025-04-13 23:34:37 +0530

    changes

--- src/Bit_Manipulation/PrintUniqueElementFromArray.java
+++ src/Bit_Manipulation/PrintUniqueElementFromArray.java
@@ -1,4 +1,15 @@
 package Bit_Manipulation;
 
 public class PrintUniqueElementFromArray {
+    public static void main(String[] args) {
+        int arr[] = {2,2,3,4,3,6,1,6,1};
+      System.out.print( printUniqueElement(arr));
+    }
+    public static int  printUniqueElement(int arr[]){
+        int res = 0;
+        for(int i = 0 ; i<arr.length;i++ ){
+             res  = res ^ arr[i];
+        }
+        return res;
+    }
 }
