09d7c4107d3b209537c95b823f249010391596c6
Saurabh Srivastav <saurabhsri.mau@gmail.com.>
2025-04-15 02:00:52 +0530

    linear search

--- src/Arrays/LinearSearch/linearSeach.java
+++ src/Arrays/LinearSearch/linearSeach.java
@@ -2,15 +2,18 @@ package Arrays.LinearSearch;
 
 public class linearSeach {
     public static void main(String[] args)  {
-        int arr[] = {2,1,5,6,8};
-       linearSearch(arr,9);
+        int arr[] = {2,2,1,5,6,8,2};
+       linearSearch(arr,2,false);
+        linearSearch(arr,2,true);
     }
-    public static void linearSearch(int arr[] ,int target){
+    public static void linearSearch(int arr[] ,int target,boolean findlast){
         int ans = -1;
        for(int i = 0;i<arr.length;i++){
            if(arr[i] == target){
              ans = i;
-             break;
+             if (findlast == false){  //toh pehli baar milne par hi loop break kar do — yeh first occurrence dikhata hai.
+                 break;
+             }
            }
        }
         if (ans == -1) {
