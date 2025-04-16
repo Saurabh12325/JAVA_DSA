bcb245728d5665dba443447f09756605cd326af4
Saurabh Srivastav <saurabhsri.mau@gmail.com.>
2025-04-17 05:09:44 +0530

    changes

--- src/MATH_DSA/SieveAlgorithm.java
+++ src/MATH_DSA/SieveAlgorithm.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 public class SieveAlgorithm {
     public static void main(String[] args) {
            SieveAlgo(100);
+
     }
     public static void SieveAlgo(int num){
         boolean arr[] = new  boolean[num+1];
@@ -24,5 +25,36 @@ public class SieveAlgorithm {
             System.out.println(i + "is" + arr[i]);
         }
     }
+//
 
-}
+
+   //Another method using the Seive algorith
+        public int countPrimes(int n) {
+            if (n <= 2) return 0;
+
+            boolean[] arr = new boolean[n];
+            Arrays.fill(arr, true);
+
+            arr[0] = arr[1] = false;
+
+            int sqrt = (int) Math.sqrt(n);
+            for (int i = 2; i <= sqrt; i++) {
+                if (arr[i]) {
+                    for (int j = i * i; j < n; j += i) {
+                        arr[j] = false;
+                    }
+                }
+            }
+
+            int count = 0;
+            for (int i = 2; i < n; i++) {
+                if (arr[i]) {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+    }
+
+
