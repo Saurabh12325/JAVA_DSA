9b5fbcb5fccdc4bf07b0648b1234f1fd93b22b6b
Saurabh Srivastav <saurabhsri.mau@gmail.com.>
2025-04-20 02:04:22 +0530

    changes

--- src/Arrays/BinarySearch/BinarySearch.java
+++ src/Arrays/BinarySearch/BinarySearch.java
@@ -3,8 +3,25 @@ package Arrays.BinarySearch;
 public class BinarySearch {
     public static void main(String[] args) {
         int arr[] = {2,5,6,8,9};
-       binarySearch(arr,4);
+        int arr2[] = {9,8,7,6,5};
+//       binarySearch(arr,9);
+//       binarySearchdec(arr2,9);
+        binarySearchAgnostic(arr ,9);
+        binarySearchAgnostic(arr2,9);
+
     }
+
+    private static void binarySearchAgnostic(int[] arr2, int target) {
+        int start = 0;
+        int end = arr2.length-1;
+        if(arr2[start] < arr2[end]){
+            binarySearch(arr2,target);
+        }else {
+            binarySearchdec(arr2,target);
+        }
+
+    }
+
     public static void binarySearch(int arr[],int target){
         int start = 0;
         int end  = arr.length-1;
@@ -25,7 +42,32 @@ public class BinarySearch {
             System.out.println("No element found in this array");
         }
         else {
-            System.out.println(ans);
+            System.out.println(" element found at increasing " +  ans);
+        }
+    }
+
+// when the array is in the decreasing order
+    public static void binarySearchdec(int arr2[],int target){
+        int start = 0;
+        int end  = arr2.length-1;
+        int ans  = -1;
+        while (start<=end){
+            int mid  = start + (end - start)/2;
+            if(arr2[mid] == target){
+                ans = mid;
+                break;
+            } else if (arr2[mid] < target) {
+                end = mid-1;
+            }else {
+              start = mid +1;
+            }
+
+        }
+        if(ans == -1){
+            System.out.println("No element found in this array");
+        }
+        else {
+            System.out.println(" element found at decreasing "+  ans);
         }
     }
 }
