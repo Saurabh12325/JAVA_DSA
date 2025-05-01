77af73355e9b24e71aa9b7a6d3e8e046dfa62320
Saurabh Srivastav <saurabhsri.mau@gmail.com.>
2025-05-02 00:44:31 +0530

    minabsdifference

---
+++ src/Arrays/BinarySearch/MinAbsoluteDifference.java
@@ -0,0 +1,40 @@
+package Arrays.BinarySearch;
+
+public class MinAbsoluteDifference {
+    public static void main(String[] args) {
+   int arr[] = {2,4,8,13,17,19};
+   minAbsolute(arr,1);
+    }
+
+    public static void minAbsolute(int arr[],int target){
+        int ans = -1;
+        int first = 0;
+        int last = arr.length-1;
+        while (first<=last){
+            int mid = first + (last-first)/2;
+            if(arr[mid] == target){
+                ans = mid;
+                break;
+            } else if (arr[mid]<target) {
+                first = mid+1;
+
+            }else {
+                last = mid-1;
+            }
+        }
+        int res = 0;
+        if(ans == -1){
+            if(last == -1){
+                res = Math.abs(target - arr[first]);
+            } else if (first == arr.length) {
+                res = Math.abs(target - arr[last]);
+            }else {
+                int ans1 = Math.abs(target - arr[first]);
+                int ans2 = Math.abs(target - arr[last]);
+                res = ans1>ans2?ans1:ans2;
+            }
+        }
+        System.out.println("Minimum Absolute Difference is " + res);
+    }
+
+}
