c8042bbdf2532d2da954aabfb55d014043f4335a
Saurabh Srivastav <saurabhsri.mau@gmail.com.>
2025-08-16 15:35:32 +0530

    Bitonic Array

--- src/BitonicArray/Bitonic.java
+++ src/BitonicArray/Bitonic.java
@@ -1,4 +1,34 @@
 package BitonicArray;
 
 public class Bitonic {
+    public static boolean validateBitonic(int arr[]){
+        int i = 0;
+        int n = arr.length;
+
+        // for increament to the peak element
+        while(i<n-1){
+            if(arr[i] < arr[i+1]){
+                i++;
+            }else {
+                break;
+            }
+        }
+        if(i == 0 || i == n-1){
+            return false;
+        }
+        while(i<n-1){
+            if(arr[i] > arr[i+1]){
+                i++;
+            }else {
+                break;
+            }
+        }
+        return (i == n-1);
+    }
+    public static void main(String[] args) {
+        int arr [] = {0,3,2,1};
+      System.out.println(validateBitonic(arr));
+    }
+
+
 }
