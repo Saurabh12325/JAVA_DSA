c8ba6f5f59e2cb14fa3b4b4037a2834000f60b49
Saurabh Srivastav <saurabhsri.mau@gmail.com.>
2025-09-02 00:17:05 +0530

    changes

--- src/BinarySearch/SingleElement.java
+++ src/BinarySearch/SingleElement.java
@@ -1,4 +1,16 @@
 package BinarySearch;
 
 public class SingleElement {
+    public static int SingleNonDuplicate(int nums[]){
+        int start = 0;
+        int end  = nums.length-2;
+        while(start <=end){
+            int mid = start + (end -start)/2;
+            if(nums[mid]== nums[mid^1]){
+                end = mid - 1;
+            }
+            start = mid + 1;
+        }
+        return nums[start];
+    }
 }
