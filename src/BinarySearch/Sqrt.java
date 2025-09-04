be2bfd1b8b109cba531e5006fc709551bfe58b9d
Saurabh Srivastav <saurabhsri.mau@gmail.com.>
2025-09-04 19:42:28 +0530

    changes

--- src/BinarySearch/Sqrt.java
+++ src/BinarySearch/Sqrt.java
@@ -1,4 +1,25 @@
 package BinarySearch;
 
-public class Sqrt {
+class Sqrt  {
+    public int mySqrt(int x) {
+        int start  = 1;
+        int end  = x;
+        while(start <= end){
+            int mid = start + (end -start)/2;
+            if(mid <= x/mid){
+                if((x % mid == 0) && mid == (x/mid)){
+                    return mid;
+                }
+                else{
+                    start = mid + 1;
+                }
+            }
+            else{
+                end = mid -1;
+            }
+
+
+        }
+        return end;
+    }
 }
---
+++ src/JumpSearch.java
@@ -0,0 +1,9 @@
+
+public class JumpSearch {
+    public static void main(String[] args) {
+
+    }
+}
+    public static  int jumpSearch(int arr[]){
+
+}
